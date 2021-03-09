d663f085ec84372aee32667bb00c35f3843154dc
varunjain3 <varun.jain@iitgn.ac.in>
2021-03-10 02:44:13 +0530

    orb+BFMatcher

---
+++ FeatureMatching.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+import cv2
+import numpy as np
+import os
+
+
+def matchfeatures(img1, img2, nfeatures=1000):
+    orb = cv2.ORB_create(nfeatures=nfeatures, scoreType=cv2.ORB_FAST_SCORE)
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    net_img = cv2.drawMatches(img1, kp1, img2, kp2,
+                              matches[:50], None, flags=2)
+    cv2.imshow("Keypoint Matching", net_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    common_pixels = []
+    for match in matches:
+        x1y1 = kp1[match.queryIdx].pt
+        x2y2 = kp2[match.trainIdx].pt
+        pixel = list(map(int, list(x1y1) + list(x1y1) + [match.distance]))
+        common_pixels.append(pixel)
+
+    return np.array(common_pixels)
+
+
+if __name__ == '__main__':
+    IMG_DIR = r'C:\Users\jaina\Google Drive\Class\6th Sem\CV\Assignment1\PanoramaStitching\Images_Asgnmt3_1\I1'
+    N = 2
+    Images = []
+    for root, dirs, files in os.walk(IMG_DIR):
+        for i in range(N):
+            img = cv2.imread(os.path.join(IMG_DIR, files[i]))
+            img = cv2.resize(img, (800, 600))
+            Images.append(img)
+
+    # for i, img in enumerate(Images):
+    #     cv2.imshow(f'Image {i+1}', img)
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+
+    a = matchfeatures(Images[0], Images[1])
+
+    print(hello)
